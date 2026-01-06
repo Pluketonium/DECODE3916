5af2d4aefd1f95be265d4dab12031073a9f2de0f
Pluketonium <lhmanns@students.wcpss.net>
2026-01-06 15:06:52 -0500

    Commit 1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Devhelp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import java.util.function.BooleanSupplier;
+
+public class Devhelp {
+    public void bindmotor(HardwareMap hardwareMap, String Motor, BooleanSupplier button, Boolean forward, BooleanSupplier running){
+        DcMotor temp = hardwareMap.get(DcMotor.class, Motor);
+        temp.setDirection(forward? DcMotor.Direction.FORWARD: DcMotor.Direction.REVERSE);
+        while (running.getAsBoolean()){
+            temp.setPower(button.getAsBoolean()?1.0:0);
+        }
+    }
+}
